c3c3a623682c3b7b36fff3e6c228203eab00e1c6
Kiea Seok Kang <grtlinux@gmail.com>
2017-04-10 17:04:33 +0900

    .

--- APP_LUCYCRON_01/project/lucycron1.01/bin/__env.cmd
+++ APP_LUCYCRON_01/project/lucycron1.01/bin/__env.cmd
@@ -8,8 +8,11 @@ if not ""%1"" == ""__env.cmd"" goto label_exit
 :label_env
 if not "%TAIN_HOME%" == "" goto label_end
 
-set TAIN_HOME=N:\PROG\tain\tools
-set LUCY_HOME=%TAIN_HOME%
+set TAIN_HOME=N:\tain
+set TAIN_TOOL=%TAIN_HOME%\tools
+set TAIN_TEST=%TAIN_HOME%\test
+
+set LUCY_HOME=%TAIN_TEST%\LucyCron\lucycron1.01
 set JAVA_HOME=%TAIN_HOME%\jdk1.7.0_79
 set CATALINA_HOME=%TAIN_HOME%\apache-tomcat-7.0.75
 
@@ -18,11 +21,14 @@ set PATH=%JAVA_HOME%\bin;%CATALINA_HOME%\bin;%PATH%
 ::----------------------------------------------
 :label_start
 echo TAIN_HOME=%TAIN_HOME%
+echo TAIN_TOOL=%TAIN_TOOL%
+echo TAIN_TEST=%TAIN_TEST%
 echo -------------------------------
 echo LUCY_HOME=%LUCY_HOME%
 echo JAVA_HOME=%JAVA_HOME%
 echo CATALINA_HOME=%CATALINA_HOME%
 echo -------------------------------
+echo PATH=%PATH%
 echo CURRENT_DIR=%CURRENT_DIR%
 echo ENV_CMD=%ENV_CMD%
 echo The environment is OK!!!

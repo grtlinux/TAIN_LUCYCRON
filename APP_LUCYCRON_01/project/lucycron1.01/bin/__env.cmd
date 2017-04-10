036ebbbeb3557bf34dfa3f9310230b971a05e8f1
Kiea Seok Kang <grtlinux@gmail.com>
2017-04-10 17:13:42 +0900

    .

--- APP_LUCYCRON_01/project/lucycron1.01/bin/__env.cmd
+++ APP_LUCYCRON_01/project/lucycron1.01/bin/__env.cmd
@@ -8,27 +8,37 @@ if not ""%1"" == ""__env.cmd"" goto label_exit
 :label_env
 if not "%TAIN_HOME%" == "" goto label_end
 
+:: base env
 set TAIN_HOME=N:\tain
 set TAIN_TOOL=%TAIN_HOME%\tools
 set TAIN_TEST=%TAIN_HOME%\test
+set TAIN_PRODUCT=%TAIN_HOME%\products
 
-set LUCY_HOME=%TAIN_TEST%\LucyCron\lucycron1.01
-set JAVA_HOME=%TAIN_HOME%\jdk1.7.0_79
-set CATALINA_HOME=%TAIN_HOME%\apache-tomcat-7.0.75
+:: tool env
+set JAVA_HOME=%TAIN_TOOL%\jdk1.7.0_79
+set CATALINA_HOME=%TAIN_TOOL%\apache-tomcat-7.0.75
 
+:: product env
+set LUCY_HOME=%TAIN_PRODUCT%\LucyCron\lucycron1.01
+
+:: other env
 set PATH=%JAVA_HOME%\bin;%CATALINA_HOME%\bin;%PATH%
 
 ::----------------------------------------------
 :label_start
+echo ----- base env --------------------------
 echo TAIN_HOME=%TAIN_HOME%
 echo TAIN_TOOL=%TAIN_TOOL%
 echo TAIN_TEST=%TAIN_TEST%
-echo -------------------------------
-echo LUCY_HOME=%LUCY_HOME%
+echo TAIN_PRODUCT=%TAIN_PRODUCT%
+echo ----- tool env --------------------------
 echo JAVA_HOME=%JAVA_HOME%
 echo CATALINA_HOME=%CATALINA_HOME%
-echo -------------------------------
+echo ----- products env --------------------------
+echo LUCY_HOME=%LUCY_HOME%
+echo ----- path env --------------------------
 echo PATH=%PATH%
+echo ----- other env --------------------------
 echo CURRENT_DIR=%CURRENT_DIR%
 echo ENV_CMD=%ENV_CMD%
 echo The environment is OK!!!

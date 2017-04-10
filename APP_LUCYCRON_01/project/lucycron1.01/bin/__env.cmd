9a1b7773e0e1f689d661e744817b45cd387d9632
Kiea Seok Kang <grtlinux@gmail.com>
2017-04-10 19:23:41 +0900

    .

--- APP_LUCYCRON_01/project/lucycron1.01/bin/__env.cmd
+++ APP_LUCYCRON_01/project/lucycron1.01/bin/__env.cmd
@@ -20,11 +20,14 @@ set LUCY_JOB_HOME=%LUCY_HOME%\lucycron1.01
 set TEST_DIR=%LUCY_HOME%\test
 set LOG_DIR=%LUCY_HOME%\logs
 set TOOL_DIR=%LUCY_HOME%\tools
+
 set JAVA_HOME=%TOOL_DIR%\jdk\jdk1.7.0_79
 set JRE_HOME=%TOOL_DIR%\jre\jre1.7.0_79
 set CATALINA_HOME=%TOOL_DIR%\apache\apache-tomcat-7.0.75
 set DERBY_HOME=%TOOL_DIR%\db-derby\db10.8
 set DERBY_OPTS=-Dderby.system.home=%LUCY_HOME%\data\derbyDB
+set NSSM_HOME=%TOOL_DIR%\nssm-2.24
+set UNX_HOME=%TOOL_DIR%\UnxUpdates
 
 :: path env
 set PATH=%JRE_HOME%\bin;%PATH%
@@ -32,6 +35,8 @@ set PATH=%JAVA_HOME%\bin;%PATH%
 set PATH=%CATALINA_HOME%\bin;%PATH%
 set PATH=%DERBY_HOME%\bin;%PATH%
 set PATH=%LUCY_JOB_HOME%\bin;%PATH%
+set PATH=%NSSM_HOME%\win64;%PATH%
+set PATH=%UNX_HOME%;%PATH%
 
 ::----------------------------------------------
 :label_start
@@ -53,6 +58,8 @@ echo JAVA_HOME=%JAVA_HOME%
 echo CATALINA_HOME=%CATALINA_HOME%
 echo DERBY_HOME=%DERBY_HOME%
 echo DERBY_OPTS=%DERBY_OPTS%
+echo NSSM_HOME=%NSSM_HOME%
+echo UNX_HOME=%UNX_HOME%
 echo ----- path env --------------------------
 echo PATH=%PATH%
 echo The environment is OK!!!
@@ -63,6 +70,8 @@ echo "##### jre version > java -version"
 echo "##### jdk version > javac -version"
 echo "##### apache-tomcat version > version"
 echo "##### derby information > sysinfo"
+echo "##### nssm windows service > nssm"
+echo "##### UnxUpdates command > tail"
 pause
 
 goto label_end

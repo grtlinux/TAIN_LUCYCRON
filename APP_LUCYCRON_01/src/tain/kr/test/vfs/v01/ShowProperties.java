f4d414416b118697fe5b0c0f01f740156cfe004e
Kiea Seok Kang <grtlinux@gmail.com>
2017-04-12 04:43:37 +0900

    .

--- APP_LUCYCRON_01/src/tain/kr/test/vfs/v01/ShowProperties.java
+++ APP_LUCYCRON_01/src/tain/kr/test/vfs/v01/ShowProperties.java
@@ -87,64 +87,58 @@ public class ShowProperties {
 
 		if (flag) {
 
-			if (args.length == 0)
-			{
+			if (args.length == 0) {
 				System.err.println("Please pass the name of a file as parameter.");
 				System.err.println("e.g. java org.apache.commons.vfs2.example.ShowProperties LICENSE.txt");
 				return;
 			}
-			for (final String arg : args)
-			{
-				try
-				{
+			
+			for (final String arg : args) {
+				try {
 					final FileSystemManager mgr = VFS.getManager();
 					System.out.println();
-					System.out.println("Parsing: " + arg);
+					System.out.println("Parsing   : " + arg);
 					final FileObject file = mgr.resolveFile(arg);
-					System.out.println("URL: " + file.getURL());
-					System.out.println("getName(): " + file.getName());
-					System.out.println("BaseName: " + file.getName().getBaseName());
-					System.out.println("Extension: " + file.getName().getExtension());
-					System.out.println("Path: " + file.getName().getPath());
-					System.out.println("Scheme: " + file.getName().getScheme());
-					System.out.println("URI: " + file.getName().getURI());
-					System.out.println("Root URI: " + file.getName().getRootURI());
-					System.out.println("Parent: " + file.getName().getParent());
-					System.out.println("Type: " + file.getType());
-					System.out.println("Exists: " + file.exists());
-					System.out.println("Readable: " + file.isReadable());
-					System.out.println("Writeable: " + file.isWriteable());
-					System.out.println("Root path: " + file.getFileSystem().getRoot().getName().getPath());
-					if (file.exists())
-					{
-						if (file.getType().equals(FileType.FILE))
-						{
+					System.out.println("URL       : " + file.getURL());
+					System.out.println("getName() : " + file.getName());
+					System.out.println("BaseName  : " + file.getName().getBaseName());
+					System.out.println("Extension : " + file.getName().getExtension());
+					System.out.println("Path      : " + file.getName().getPath());
+					System.out.println("Scheme    : " + file.getName().getScheme());
+					System.out.println("URI       : " + file.getName().getURI());
+					System.out.println("Root URI  : " + file.getName().getRootURI());
+					System.out.println("Parent    : " + file.getName().getParent());
+					System.out.println("Type      : " + file.getType());
+					System.out.println("Exists    : " + file.exists());
+					System.out.println("Readable  : " + file.isReadable());
+					System.out.println("Writeable : " + file.isWriteable());
+					System.out.println("Root path : " + file.getFileSystem().getRoot().getName().getPath());
+					
+					if (file.exists()) {
+						if (file.getType().equals(FileType.FILE)) {
 							System.out.println("Size: " + file.getContent().getSize() + " bytes");
-						}
-						else if (file.getType().equals(FileType.FOLDER) && file.isReadable())
-						{
+							
+						} else if (file.getType().equals(FileType.FOLDER) && file.isReadable()) {
+							
 							final FileObject[] children = file.getChildren();
 							System.out.println("Directory with " + children.length + " files");
-							for (int iterChildren = 0; iterChildren < children.length; iterChildren++)
-							{
+							
+							for (int iterChildren = 0; iterChildren < children.length; iterChildren++) {
 								System.out.println("#" + iterChildren + ": " + children[iterChildren].getName());
-								if (iterChildren > SHOW_MAX)
-								{
+								if (iterChildren > SHOW_MAX) {
 									break;
 								}
 							}
 						}
+						
 						System.out.println("Last modified: " + DateFormat.getInstance().format(
 								new Date(file.getContent().getLastModifiedTime())));
-					}
-					else
-					{
+					} else {
 						System.out.println("The file does not exist");
 					}
+					
 					file.close();
-				}
-				catch (final FileSystemException ex)
-				{
+				} catch (final FileSystemException ex) {
 					ex.printStackTrace();
 				}
 			}
@@ -160,6 +154,8 @@ public class ShowProperties {
 			log.debug(">>>>> " + new Object() {
 			}.getClass().getEnclosingClass().getName());
 
+		args = new String[] { "N:/tain/test" };
+		
 		if (flag)
 			test01(args);
 	}

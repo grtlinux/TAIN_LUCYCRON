8430d5af064499bd39f35536923f97315ee3ecfe
Kiea Seok Kang <grtlinux@gmail.com>
2017-04-12 04:37:42 +0900

    .

--- APP_LUCYCRON_01/src/tain/kr/test/vfs/v01/ChangeLastModificationTime.java
+++ APP_LUCYCRON_01/src/tain/kr/test/vfs/v01/ChangeLastModificationTime.java
@@ -19,13 +19,7 @@
  */
 package tain.kr.test.vfs.v01;
 
-import java.text.DateFormat;
-import java.util.Date;
-
 import org.apache.commons.vfs2.FileObject;
-import org.apache.commons.vfs2.FileSystemException;
-import org.apache.commons.vfs2.FileSystemManager;
-import org.apache.commons.vfs2.FileType;
 import org.apache.commons.vfs2.VFS;
 import org.apache.log4j.Logger;
 
@@ -51,10 +45,6 @@ public final class ChangeLastModificationTime {
 			.getLogger(ChangeLastModificationTime.class);
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
-	
-	/** Maximum number of children to show. */
-	private static final int SHOW_MAX = 5;
-
 	///////////////////////////////////////////////////////////////////////////////////////////////
 
 	/*
@@ -88,67 +78,16 @@ public final class ChangeLastModificationTime {
 
 		if (flag) {
 
-			if (args.length == 0)
-			{
+			if (args.length == 0) {
 				System.err.println("Please pass the name of a file as parameter.");
-				System.err.println("e.g. java org.apache.commons.vfs2.example.ShowProperties LICENSE.txt");
 				return;
 			}
-			for (final String arg : args)
-			{
-				try
-				{
-					final FileSystemManager mgr = VFS.getManager();
-					System.out.println();
-					System.out.println("Parsing: " + arg);
-					final FileObject file = mgr.resolveFile(arg);
-					System.out.println("URL: " + file.getURL());
-					System.out.println("getName(): " + file.getName());
-					System.out.println("BaseName: " + file.getName().getBaseName());
-					System.out.println("Extension: " + file.getName().getExtension());
-					System.out.println("Path: " + file.getName().getPath());
-					System.out.println("Scheme: " + file.getName().getScheme());
-					System.out.println("URI: " + file.getName().getURI());
-					System.out.println("Root URI: " + file.getName().getRootURI());
-					System.out.println("Parent: " + file.getName().getParent());
-					System.out.println("Type: " + file.getType());
-					System.out.println("Exists: " + file.exists());
-					System.out.println("Readable: " + file.isReadable());
-					System.out.println("Writeable: " + file.isWriteable());
-					System.out.println("Root path: " + file.getFileSystem().getRoot().getName().getPath());
-					if (file.exists())
-					{
-						if (file.getType().equals(FileType.FILE))
-						{
-							System.out.println("Size: " + file.getContent().getSize() + " bytes");
-						}
-						else if (file.getType().equals(FileType.FOLDER) && file.isReadable())
-						{
-							final FileObject[] children = file.getChildren();
-							System.out.println("Directory with " + children.length + " files");
-							for (int iterChildren = 0; iterChildren < children.length; iterChildren++)
-							{
-								System.out.println("#" + iterChildren + ": " + children[iterChildren].getName());
-								if (iterChildren > SHOW_MAX)
-								{
-									break;
-								}
-							}
-						}
-						System.out.println("Last modified: " + DateFormat.getInstance().format(
-								new Date(file.getContent().getLastModifiedTime())));
-					}
-					else
-					{
-						System.out.println("The file does not exist");
-					}
-					file.close();
-				}
-				catch (final FileSystemException ex)
-				{
-					ex.printStackTrace();
-				}
-			}
+
+			final FileObject fo = VFS.getManager().resolveFile(args[0]);
+			final long setTo = System.currentTimeMillis();
+			System.err.println("set to: " + setTo);
+			fo.getContent().setLastModifiedTime(setTo);
+			System.err.println("after set: " + fo.getContent().getLastModifiedTime());
 		}
 	}
 
@@ -161,6 +100,8 @@ public final class ChangeLastModificationTime {
 			log.debug(">>>>> " + new Object() {
 			}.getClass().getEnclosingClass().getName());
 
+		args = new String[] { "N:/tain/test" };
+		
 		if (flag)
 			test01(args);
 	}

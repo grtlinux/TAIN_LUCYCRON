a1d19d1c2b5c928adb84251575bcadbccf638f6d
Kiea Seok Kang <grtlinux@gmail.com>
2017-04-14 05:28:19 +0900

    .

---
+++ APP_LUCYCRON_01/project/lucycron1.01/bin/stopDb.bat
@@ -0,0 +1,11 @@
+@echo off
+@setlocal
+
+::----------------------------------------------
+:label_start
+start stopNetworkServer.bat -p 1527 -user kang -password kang123!
+
+::----------------------------------------------
+:label_end
+
+@endlocal

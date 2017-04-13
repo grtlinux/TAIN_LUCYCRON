a8200ef7c2d1d87c95e1d4b452172bf1a48dab53
Kiea Seok Kang <grtlinux@gmail.com>
2017-04-14 07:17:14 +0900

    .

--- APP_LUCYCRON_01/project/lucycron1.01/sql/Script_lucycron01.sql
+++ APP_LUCYCRON_01/project/lucycron1.01/sql/Script_lucycron01.sql
@@ -4,85 +4,15 @@
 --
 
 -- ========================================================================================
--- TB_CPUINFO
+-- TB_RUNCWD
 
-SELECT COUNT(*) FROM TB_RUNCWD;
-
-SELECT * FROM TB_CPUINFO WHERE F_YN = 'Y';
-
-
-
-
-
-
--- ========================================================================================
--- TB_CPUREC
-
-SELECT COUNT(*) FROM TB_CPUREC;
-
-SELECT * FROM TB_CPUREC WHERE F_CPUNM = 'TOTAL';
-
-SELECT TIME(F_DTTM), CEIL(10000 - F_IDL*10000) / 100 AS CPU_USE FROM TB_CPUREC WHERE F_CPUNM = 'TOTAL';
-
-
-
-
--- ========================================================================================
--- TB_MEMREC
-
-SELECT COUNT(*) FROM TB_MEMREC;
-
-SELECT * FROM TB_MEMREC;
-
-SELECT
-	CURRENT_TIME,
-	TIME(F_DTTM), 
-	DOUBLE(F_AUSE)/1024/1024/1024, 
-	DOUBLE(F_USE)/1024/1024/1024, 
-	--F_TTL, 
-	--F_FRE, 
-	--F_USE, 
-	--F_AFRE, 
-	--F_AUSE, 
-	CEIL(F_FREP * 100) / 100, 
-	CEIL(F_USEP * 100) / 100 
-FROM
-	TB_MEMREC
-ORDER BY
-	F_DTTM DESC
---FETCH FIRST ROW ONLY
---FETCH NEXT 10 ROWS ONLY
---OFFSET 100 ROWS
---OFFSET 10 ROWS FETCH NEXT 10 ROWS ONLY
---FETCH FIRST 10 ROWS ONLY
-OFFSET 0 ROWS FETCH NEXT 10 ROWS ONLY
-;
-
-
-
-
-
-
-
-
--- ========================================================================================
--- TB_DSKREC
-
-SELECT COUNT(*) FROM TB_DSKREC;
-
-SELECT * FROM TB_DSKREC;
-
-
-
-
-
--- ========================================================================================
--- UNION
-
-SELECT 'TB_CPUINFO' AS NAME, COUNT(*) AS TB_CNT FROM TB_CPUINFO
-UNION SELECT 'TB_CPUREC' AS NAME, COUNT(*) AS TB_CNT FROM TB_CPUREC
-UNION SELECT 'TB_MEMREC' AS NAME, COUNT(*) AS TB_CNT FROM TB_MEMREC
-UNION SELECT 'TB_DSKREC' AS NAME, COUNT(*) AS TB_CNT FROM TB_DSKREC
+SELECT 'TB_RUNCWD' AS TB_NAME, COUNT(*) AS TB_CNT FROM TB_RUNCWD
+UNION SELECT 'TB_RUNCMD'  AS TB_NAME, COUNT(*) AS TB_CNT FROM TB_RUNCMD
+UNION SELECT 'TB_RUNENV'  AS TB_NAME, COUNT(*) AS TB_CNT FROM TB_RUNENV
+UNION SELECT 'TB_RUNHHMM' AS TB_NAME, COUNT(*) AS TB_CNT FROM TB_RUNHHMM
+UNION SELECT 'TB_RUNPROP' AS TB_NAME, COUNT(*) AS TB_CNT FROM TB_RUNPROP
+UNION SELECT 'TB_SCHDTTM' AS TB_NAME, COUNT(*) AS TB_CNT FROM TB_SCHDTTM
+UNION SELECT 'TB_SCHLOG'  AS TB_NAME, COUNT(*) AS TB_CNT FROM TB_SCHLOG
 ;
 
 
@@ -93,31 +23,6 @@ UNION SELECT 'TB_DSKREC' AS NAME, COUNT(*) AS TB_CNT FROM TB_DSKREC
 
 
 
-
--- ========================================================================================
--- ========================================================================================
--- ========================================================================================
--- FEPGRP, FEPINFO
-
-SELECT 'TB_FEPGRP' AS NAME, COUNT(*) AS TB_CNT FROM KANG.TB_FEPGRP
-UNION SELECT 'TB_FEPINFO' AS NAME, COUNT(*) AS TB_CNT FROM KANG.TB_FEPINFO
-;
-
-
-SELECT
-	GRP.F_GRPID
-	, GRP.F_GRPNM
-	, INFO.F_FEPID
-	, INFO.F_FEPNM
-	, INFO.F_TYPNM
-	, INFO.F_DESC
-	, INFO.F_CMMT
-FROM
-	KANG.TB_FEPGRP GRP
-	, KANG.TB_FEPINFO INFO
-WHERE
-	GRP.F_GRPID = INFO.F_GRPID
-;
 
 
 

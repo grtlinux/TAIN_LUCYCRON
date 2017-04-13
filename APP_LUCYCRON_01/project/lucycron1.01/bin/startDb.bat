fba6a60caf3d29ae23858738bc53c405787ae075
Kiea Seok Kang <grtlinux@gmail.com>
2017-04-14 05:28:14 +0900

    .

---
+++ APP_LUCYCRON_01/project/lucycron1.01/bin/startDb.bat
@@ -0,0 +1,11 @@
+@echo off
+@setlocal
+
+::----------------------------------------------
+:label_start
+start startNetworkServer.bat
+
+::----------------------------------------------
+:label_end
+
+@endlocal

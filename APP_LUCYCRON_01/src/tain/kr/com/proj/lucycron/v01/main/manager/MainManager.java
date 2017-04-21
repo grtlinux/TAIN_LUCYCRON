fa02cc942891cdadbef164f1cf26b18a5b624c47
Kiea Seok Kang <grtlinux@gmail.com>
2017-04-22 00:42:19 +0900

    .

---
+++ APP_LUCYCRON_01/src/tain/kr/com/proj/lucycron/v01/main/manager/MainManager.java
@@ -0,0 +1,118 @@
+/**
+ * Copyright 2014, 2015, 2016, 2017 TAIN, Inc. all rights reserved.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * Copyright 2014, 2015, 2016, 2017 TAIN, Inc.
+ *
+ */
+package tain.kr.com.proj.lucycron.v01.main.manager;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Code Templates > Comments > Types
+ *
+ * <PRE>
+ *   -. FileName   : MainManager.java
+ *   -. Package    : tain.kr.com.proj.lucycron.v01.main.manager
+ *   -. Comment    :
+ *   -. Author     : taincokr
+ *   -. First Date : 2017. 4. 22. {time}
+ * </PRE>
+ *
+ * @author taincokr
+ *
+ */
+public final class MainManager {
+
+	private static boolean flag = true;
+
+	private static final Logger log = Logger.getLogger(MainManager.class);
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	/*
+	 * constructor
+	 */
+	public MainManager() {
+		if (flag)
+			log.debug(">>>>> in class " + this.getClass().getSimpleName());
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public void manage() throws Exception {
+		
+		if (flag) log.debug("##### start manager.manage() #####");
+		
+		if (flag) {
+			/*
+			 * begin
+			 */
+		}
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private static MainManager instance = null;
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public static synchronized MainManager getInstance() throws Exception {
+		
+		if (MainManager.instance == null) {
+			MainManager.instance = new MainManager();
+		}
+		
+		return MainManager.instance;
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	/*
+	 * static test method
+	 */
+	private static void test01(String[] args) throws Exception {
+
+		if (flag) {
+			/*
+			 * begin
+			 */
+			MainManager.getInstance().manage();
+		}
+	}
+
+	/*
+	 * main method
+	 */
+	public static void main(String[] args) throws Exception {
+
+		if (flag)
+			log.debug(">>>>> " + new Object() {
+			}.getClass().getEnclosingClass().getName());
+
+		if (flag)
+			test01(args);
+	}
+}

3fac5fc1970c4aca6b78490781292ecd2a4429bc
Kiea Seok Kang <grtlinux@gmail.com>
2017-04-23 06:23:03 +0900

    .

--- APP_LUCYCRON_01/src/tain/kr/com/proj/lucycron/v01/test/TestFiles.java
+++ APP_LUCYCRON_01/src/tain/kr/com/proj/lucycron/v01/test/TestFiles.java
@@ -20,6 +20,8 @@
 package tain.kr.com.proj.lucycron.v01.test;
 
 import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
 
 import org.apache.log4j.Logger;
 
@@ -75,7 +77,7 @@ public final class TestFiles {
 			String[] arrStrFileNames = null;
 			
 			try {
-				file = new File(Params.getInstance().getString("tain.tab.folder"));
+				file = new File(Params.getInstance().getString("tain.lucy.tab.folder"));
 				arrStrFileNames = file.list();
 				
 				for (String strFileName : arrStrFileNames) {
@@ -90,6 +92,186 @@ public final class TestFiles {
 		}
 	}
 
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	/*
+	 * static test method
+	 */
+	private static void test02(String[] args) throws Exception {
+
+		if (flag) {
+			/*
+			 * 2. String[] File.list(FilenameFilter)
+			 */
+			System.out.println("2. String[] File.list(FilenameFilter);");
+			
+			File file = null;
+			String[] arrStrFileNames = null;
+			
+			try {
+				file = new File(Params.getInstance().getString("tain.lucy.tab.folder"));
+				arrStrFileNames = file.list(new FilenameFilter() {
+					@Override
+					public boolean accept(File dir, String name) {
+						
+						if (flag) System.out.printf("2> [%s] [%s]\n", dir.getAbsolutePath(), name);
+						if (flag) return true;
+						
+						return false;
+					}
+				});
+				
+				for (String strFileName : arrStrFileNames) {
+					System.out.printf("2> [%s]\n", strFileName);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {}
+			
+			System.out.println();
+		}
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	/*
+	 * static test method
+	 */
+	private static void test03(String[] args) throws Exception {
+
+		if (flag) {
+			/*
+			 * 3. File[] File.listFiles()
+			 */
+			System.out.println("3. File[] File.listFiles();");
+			
+			File file = null;
+			File[] arrFiles = null;
+			
+			try {
+				file = new File(Params.getInstance().getString("tain.lucy.tab.folder"));
+				arrFiles = file.listFiles();
+				
+				for (File f : arrFiles) {
+					System.out.printf("3> [%s]\n", f);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {}
+			
+			System.out.println();
+		}
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	/*
+	 * static test method
+	 */
+	private static void test04(String[] args) throws Exception {
+
+		if (flag) {
+			/*
+			 * 4. File[] File.listFiles(FileFilter)
+			 */
+			System.out.println("4. File[] File.listFiles(FileFilter);");
+			
+			File file = null;
+			File[] arrFiles = null;
+			
+			try {
+				file = new File(Params.getInstance().getString("tain.lucy.tab.folder"));
+				arrFiles = file.listFiles(new FileFilter() {
+					@Override
+					public boolean accept(File file) {
+						
+						if (flag) return true;
+						
+						return false;
+					}
+				});
+				
+				for (File f : arrFiles) {
+					System.out.printf("4> [%s]\n", f);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {}
+			
+			System.out.println();
+		}
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	/*
+	 * static test method
+	 */
+	private static void test05(String[] args) throws Exception {
+
+		if (flag) {
+			/*
+			 * 5. File[] File.listFiles(FilenameFilter)
+			 */
+			System.out.println("5. File[] File.listFiles(FilenameFilter);");
+			
+			File file = null;
+			File[] arrFiles = null;
+			
+			try {
+				file = new File(Params.getInstance().getString("tain.lucy.tab.folder"));
+				arrFiles = file.listFiles(new FilenameFilter() {
+					@Override
+					public boolean accept(File dir, String name) {
+						
+						if (flag) return true;
+						
+						return false;
+					}
+				});
+				
+				for (File f : arrFiles) {
+					System.out.printf("5> [%s]\n", f);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {}
+			
+			System.out.println();
+		}
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	/*
+	 * static test method
+	 */
+	private static void test06(String[] args) throws Exception {
+
+		if (flag) {
+			/*
+			 * 6. File[] File.listRoots()
+			 */
+			System.out.println("6. File[] File.listRoots();");
+			
+			File[] files = null;
+			
+			try {
+				files = File.listRoots();
+				
+				for (File f : files) {
+					System.out.printf("6> [%s]\n", f);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {}
+			
+			System.out.println();
+		}
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
 	/*
 	 * main method
 	 */
@@ -100,5 +282,10 @@ public final class TestFiles {
 			}.getClass().getEnclosingClass().getName());
 
 		if (flag) test01(args);
+		if (flag) test02(args);
+		if (flag) test03(args);
+		if (flag) test04(args);
+		if (flag) test05(args);
+		if (flag) test06(args);
 	}
 }

8807d04a0c315205f8667c3e10c7e45531a5e866
Kiea Seok Kang <grtlinux@gmail.com>
2017-04-23 08:30:14 +0900

    .

--- APP_LUCYCRON_01/src/tain/kr/com/proj/lucycron/v01/main/manager/MainManager.java
+++ APP_LUCYCRON_01/src/tain/kr/com/proj/lucycron/v01/main/manager/MainManager.java
@@ -19,7 +19,17 @@
  */
 package tain.kr.com.proj.lucycron.v01.main.manager;
 
+import java.io.File;
+import java.io.FileFilter;
+
 import org.apache.log4j.Logger;
+
+import tain.kr.com.proj.lucycron.v01.controller.SchController;
+import tain.kr.com.proj.lucycron.v01.controller.SchRequest;
+import tain.kr.com.proj.lucycron.v01.controller.SchRequestHandler;
+import tain.kr.com.proj.lucycron.v01.enums.OsType;
+import tain.kr.com.proj.lucycron.v01.util.CheckSystem;
+import tain.kr.com.proj.lucycron.v01.util.Params;
 
 /**
  * Code Templates > Comments > Types
@@ -42,6 +52,12 @@ public final class MainManager {
 	private static final Logger log = Logger.getLogger(MainManager.class);
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private String strOsName;
+	private boolean isWindows;
+	private String strExtName;
+	private String strFolder;
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 
 	/*
@@ -57,6 +73,13 @@ public final class MainManager {
 	public void manage() throws Exception {
 		
 		if (flag) log.debug("##### start manager.manage() #####");
+
+		if (flag) {
+			/*
+			 * initialize
+			 */
+			initialize();
+		}
 		
 		if (flag) {
 			/*
@@ -68,14 +91,117 @@ public final class MainManager {
 	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	
+	private void initialize() throws Exception {
+		
+		if (flag) {
+			this.strOsName = CheckSystem.getInstance().getOsName();
+			this.isWindows = CheckSystem.getInstance().isWindows();
+			if (this.isWindows) {
+				this.strExtName = OsType.WINDOWS.getExtName();
+			} else {
+				this.strExtName = OsType.LINUX.getExtName();
+			}
+			
+			if (flag) log.debug(String.format("[strOsName] = [%s][%s], [isWindows] = [%s]"
+					, this.strOsName, this.strExtName, this.isWindows));
+		}
+
+		if (flag) {
+			this.strFolder = Params.getInstance().getString("tain.lucy.tab.folder");
+			if (flag) log.debug(String.format("[strFolder] = [%s]", this.strFolder));
+		}
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
 	private void loadSchController() throws Exception {
 		
+		File[] arrFiles = null;
+
 		if (flag) {
 			/*
 			 * read Files on the folder of tain.lucy.tab.folder parameter value
 			 */
-			
-			
+			File folder = null;
+
+			try {
+				folder = new File(this.strFolder);
+				arrFiles = folder.listFiles(new FileFilter() {
+					@Override
+					public boolean accept(File file) {
+						
+						if (!flag) {
+							/*
+							 * print File method for checking
+							 */
+							try {
+								System.out.printf("file.getAbsolutePath()  = [%s]\n", file.getAbsolutePath());
+								System.out.printf("file.getCanonicalPath() = [%s]\n", file.getCanonicalPath());   // <- getAbsolutePath
+								System.out.printf("file.getName()          = [%s]\n", file.getName());            // <- fileName
+								System.out.printf("file.getParent()        = [%s]\n", file.getParent());          // <- path contains fileName
+								System.out.printf("file.getPath()          = [%s]\n", file.getPath());            // <- be equals to toString if fileName
+								System.out.printf("file.toString()         = [%s]\n", file.toString());
+								System.out.printf("file                    = [%s]\n", file);                      // <- toString
+								System.out.printf("file.getName().indexOf(.) = [%d]\n", file.getName().indexOf('.'));
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						}
+						
+						if (file.isDirectory()) {
+							/*
+							 * if directory, return false
+							 */
+							return false;
+						}
+
+						int idx = file.getName().indexOf('.');
+						if (idx <= 0) {
+							/*
+							 * no extension of SCHID file
+							 */
+							return false;
+						}
+						
+						if (flag) {
+							/*
+							 * check extension of SCHID file
+							 */
+							String extension = file.getName().substring(idx+1);
+							if (!extension.equals(strExtName))
+								return false;
+						}
+
+						return true;
+					}
+				});
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+			}
+
+			if (!flag) {
+				/*
+				 * print the result of above processing
+				 */
+				for (File file : arrFiles) {
+					if (flag) log.debug(String.format("[%s]\n", file.getName()));
+				}
+			}
+		}
+		
+		if (flag) {
+			/*
+			 * create request handler
+			 */
+			for (File file : arrFiles) {
+				if (flag) log.debug(String.format("[%s]", file.getName()));
+				
+				SchRequest request = new SchRequest(file.getName());
+				SchRequestHandler handler = new SchRequestHandler(request);
+				
+				SchController.getInstance().addHandler(request, handler);
+			}
 		}
 	}
 	

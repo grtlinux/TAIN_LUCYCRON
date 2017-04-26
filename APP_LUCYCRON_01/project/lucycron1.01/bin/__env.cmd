4b9cb4de0921f0b4b37d4c5638146b1411063e69
Kiea Seok Kang <grtlinux@gmail.com>
2017-04-27 02:47:05 +0900

    .

--- APP_LUCYCRON_01/project/lucycron1.01/bin/__env.cmd
+++ APP_LUCYCRON_01/project/lucycron1.01/bin/__env.cmd
@@ -27,7 +27,7 @@ set CATALINA_HOME=%TOOL_DIR%\apache\apache-tomcat-7.0.75
 set DERBY_HOME=%TOOL_DIR%\db-derby\db10.8
 set DERBY_OPTS=-Dderby.system.home=%LUCY_BASE%\data\derbyDB
 set NSSM_HOME=%TOOL_DIR%\nssm-2.24
-set UNX_HOME=%TOOL_DIR%\UnxUpdates
+::set UNX_HOME=%TOOL_DIR%\UnxUpdates
 set M2_HOME=%TOOL_DIR%\apache\apache-maven-3.3.3
 ::set M2_HOME=%TOOL_DIR%\apache\apache-maven-3.5.0
 ::set MAVEN_OPTS=-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000
@@ -40,7 +40,7 @@ set PATH=%CATALINA_HOME%\bin;%PATH%
 set PATH=%DERBY_HOME%\bin;%PATH%
 set PATH=%LUCY_HOME%\bin;%PATH%
 set PATH=%NSSM_HOME%\win64;%PATH%
-set PATH=%UNX_HOME%;%PATH%
+::set PATH=%UNX_HOME%;%PATH%
 set PATH=%M2_HOME%\bin;%PATH%
 set PATH=%ANT_HOME%\bin;%PATH%
 
@@ -68,19 +68,45 @@ echo "> explorer      : 탐색기"
 echo "> tasklist      : 프로세스 목록(ex. tasklist /fi 'pid eq 1234')"
 echo "> taskkill      : 프로세스 종료(ex. taskkill /fi 'imagename eq acme*')"
 echo ""
-echo "> control       : 제어판"
 echo "> msconfig      : 시스템 구성"
+echo "> msinfo32      : 시스템 정보"
 echo "> mstsc         : 원격 데스크톱 연결"
+echo "> perfmon       : 성능 모니터"
+echo "> resmon        : 리소스 모니터"
+echo "> control       : 제어판"
+echo "> control system         : 시스템"
+echo "> control admintools     : 관리도구"
+echo "> control folders        : 폴더 옵션"
+echo "> control desktop        : 개인 설정"
+echo "> control netconnections : 네트워크 연결"
+echo "> control printers       : 장치 및 프린터"
+echo "> control schedtasks     : 작업 스케줄러"
+echo "> control userpasswords  : 사용자 계정"
+echo "> control userpasswords2 : 사용자 계정2"
 echo ""
+echo "C:\> dir /s *.cpl : 모든 CPL(Control Panel applets) 파일 찾기"
+echo "> appwiz.cpl    : 프로그램 제거 또는 변경"
+echo "> desk.cpl      : 화면 해상도"
 echo "> firewall.cpl  : Windows 방화벽"
-echo "> ncpa.cpl      : 네트워크 연결(eq. control netconnections)"
+echo "> ncpa.cpl      : 네트워크 연결"
+echo "> sysdm.cpl     : 시스템 속성"
+echo "> hdwwiz.cpl    : 장치 관리자"
+echo "> wscui.cpl     : 관리 센터 > 최근 메시지 검토 및 문제 해결"
 echo ""
-echo "> compmgmt.msc  : 컴퓨터 관리"
-echo "> services.msc  : 서비스"
-echo "> devmgmt.msc   : 장치 관리자"
-echo "> diskmgmt.msc  : 디스크 관리"
-echo "> fsmgmt.msc    : 공유 폴더"
-
+echo "C:\> dir /s *.msc : 모든 MSC 파일 찾기"
+echo "> compmgmt.msc        : 컴퓨터 관리"
+echo "> services.msc        : 서비스"
+echo "> devmgmt.msc         : 장치 관리자"
+echo "> diskmgmt.msc        : 디스크 관리"
+echo "> fsmgmt.msc          : 공유 폴더"
+echo "> gpedit.msc          : 로컬 그룹 정책 편집기"
+echo "> lusrmgr.msc         : 로컬 사용자 및 그룹"
+echo "> secpol.msc          : 로컬 보안 정책"
+echo "> comexp.msc          : 구성 요소 서비스"
+echo "> certmgr.msc         : 인증서 - 현재 사용자"
+echo "> printmanagement.msc : 인쇄 관리"
+echo "> taskschd.msc        : 작업 스케줄러"
+echo "> wf.msc              : 고급 보안이 포함된 Windows 방화벽"
 echo "> cmd : command"
 
 echo ""

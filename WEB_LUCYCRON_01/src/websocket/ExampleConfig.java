dca6e3c8c61c53e3f67b2a8be50395824e8e222c
Kiea Seok Kang <grtlinux@gmail.com>
2017-06-20 07:34:59 +0900

    .

--- WEB_LUCYCRON_01/src/websocket/ExampleConfig.java
+++ WEB_LUCYCRON_01/src/websocket/ExampleConfig.java
@@ -19,6 +19,15 @@
  */
 package websocket;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.websocket.Endpoint;
+import javax.websocket.server.ServerApplicationConfig;
+import javax.websocket.server.ServerEndpointConfig;
+
+import websocket.echo.EchoEndpoint;
+
 /**
  * Code Templates > Comments > Types
  *
@@ -33,6 +42,37 @@ package websocket;
  * @author taincokr
  *
  */
-public class ExampleConfig {
+public class ExampleConfig implements ServerApplicationConfig {
 
+	@Override
+	public Set<ServerEndpointConfig> getEndpointConfigs(Set<Class<? extends Endpoint>> scanned) {
+		
+		Set<ServerEndpointConfig> result = new HashSet<ServerEndpointConfig>();
+		
+		if (scanned.contains(EchoEndpoint.class)) {
+			result.add(ServerEndpointConfig.Builder.create(EchoEndpoint.class, "/websocket/echoProgrammatic").build());
+		}
+		
+		//if (scanned.contains(DrawboardEndpoint.class)) {
+		//	result.add(ServerEndpointConfig.Builder.create(DrawBoardEndpoint.class, "/websocket/drawboard").build());
+		//}
+		
+		return result;
+	}
+	
+	@Override
+	public Set<Class<?>> getAnnotatedEndpointClasses(Set<Class<?>> scanned) {
+		// Deploy all WebSocket endpoints defined by annotations in the examples
+		// web application. Filter out all others to avoid issues when running
+		// tests on Gump
+		
+		Set<Class<?>> results = new HashSet<Class<?>>();
+		for (Class<?> clazz : scanned) {
+			if (clazz.getPackage().getName().startsWith("websocket.")) {
+				results.add(clazz);
+			}
+		}
+		
+		return results;
+	}
 }

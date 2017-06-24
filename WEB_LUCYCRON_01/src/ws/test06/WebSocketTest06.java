455db28b3488de36e3237c553f65d6366deebbb7
Kiea Seok Kang <grtlinux@gmail.com>
2017-06-25 05:11:10 +0900

    .

--- WEB_LUCYCRON_01/src/ws/test06/WebSocketTest06.java
+++ WEB_LUCYCRON_01/src/ws/test06/WebSocketTest06.java
@@ -19,12 +19,22 @@
  */
 package ws.test06;
 
+import java.io.IOException;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+
+import util.HTMLFilter;
 
 /**
  * Code Templates > Comments > Types
@@ -41,25 +51,72 @@ import javax.websocket.server.ServerEndpoint;
  *
  */
 @ServerEndpoint("/ws/test06")
-public class WebSocketTest06 {
+public final class WebSocketTest06 {
 
+	private final static  Log log = LogFactory.getLog(WebSocketTest06.class);
+	
+	private final static String GUEST_PREFIX = "TEST06";
+	private final static AtomicInteger connectionIDs = new AtomicInteger(0);
+	private final static Set<WebSocketTest06> connections = new CopyOnWriteArraySet<WebSocketTest06>();
+	
+	private final String nickName;
+	private Session session;
+	
+	public WebSocketTest06() {
+		log.info("INFO: constructor nickName: " + this.nickName);
+
+		this.nickName = String.format("%s-%02d", GUEST_PREFIX, connectionIDs.getAndIncrement());
+	}
+	
 	@OnOpen
 	public void onOpen(Session session) {
-		
+		log.info("INFO: OnOpen nickName: " + this.nickName);
+
+		this.session = session;
+		connections.add(this);
+		String message = String.format("* %s %s", this.nickName, "has joined.");
+		broadcast(message);
 	}
 	
 	@OnClose
 	public void onClose(Session session) {
-		
+		log.info("INFO: OnClose nickName: " + this.nickName);
+
+		connections.remove(this);
+		String message = String.format("* %s %s", this.nickName, "has disconnected.");
+		broadcast(message);
 	}
 	
 	@OnError
-	public void onError(Throwable e, Session session) {
-		
+	public void onError(Throwable e, Session session) throws Throwable {
+		log.info("INFO: OnError nickName: " + this.nickName);
+
+		log.error("ERROR: " + e.toString(), e);
 	}
 	
 	@OnMessage
 	public void onMessage(String message, Session session) {
+		log.info("INFO: OnMessage nickName: " + this.nickName);
 		
+		String filteredMessage = String.format("%s: %s", this.nickName, HTMLFilter.filter(message.toString()));
+		broadcast(filteredMessage);
+	}
+	
+	private static void broadcast(String message) {
+		synchronized (connections) {
+			for (WebSocketTest06 client : connections) {
+				try {
+					client.session.getBasicRemote().sendText(message);
+				} catch (IOException e) {
+					log.debug("ERROR: Failed to send message to client.", e);
+					connections.remove(client);
+					try {
+						client.session.close();
+					} catch (IOException e2) {
+						// ignore
+					}
+				}
+			}
+		}
 	}
 }

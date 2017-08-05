36a072972cf509ba385cea6b98b5d0e9d9fdeec0
grtlinux <grtlinuxr@gmail.com>
2017-08-06 02:44:48 +0900

    .

--- APP_LUCYCRON_01/src/tain/kr/test/ucanaccess/v00/Example.java
+++ APP_LUCYCRON_01/src/tain/kr/test/ucanaccess/v00/Example.java
@@ -19,6 +19,7 @@
  */
 package tain.kr.test.ucanaccess.v00;
 
+import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -45,6 +46,111 @@ import org.apache.log4j.Logger;
  * </PRE>
  *
  * @author taincokr
+ *
+ *
+ *     데이터 형식 크기
+ *     ----------- --------------------------------------------
+ *     텍스트      최대 255B
+ *     메모        최대 1G 문자, 2GB, 그 중 65,535는 컨트롤 표시
+ *     숫자        1B, 2B, 4B, 8B, 16B
+ *     날짜/시간   8B
+ *     통화        8B
+ *     일련번호    4B, 16B
+ *     예/아니오   1bit
+ *     OLE개체     최대 1G
+ *     첨부파일
+ *     하이퍼링크
+ *     조회마법사
+ *
+ *
+ *     1. CREATE TABLE 문을 이용하여 테이블을 만듭니다.
+ *
+ *         CREATE TABLE 테이블
+ *         (
+ *             ID          AUTOINCREMENT(1,1)
+ *                         CONSTRAINT PK_테이블 PRIMARY KEY,
+ *             이름        TEXT(50) NOT NULL,
+ *             전화번호    TEXT(50) NOT NULL,
+ *             주소        TEXT(100) NOT NULL,
+ *             이메일      TEXT(50)
+ *         )
+ *
+ *     2. DROP TABLE 문을 이용하여 테이블을 삭제합니다.
+ *
+ *         DROP TABLE 테이블
+ *
+ *     3. ALTER TABLE 문을 이용하여 테이블을 수정합니다.
+ *
+ *         ALTER TABLE 테이블 ADD COLUMN 우편번호 TEXT(20)
+ *         ALTER TABLE 테이블 ALTER COLUMN 우편번호 TEXT(15)
+ *         ALTER TABLE 테이블 DROP COLUMN 우편번호
+ *
+ *     4. CREATE INDEX 문을 이용하여 테이블의 기본키 인덱스를 추가합니다.
+ *
+ *         CREATE INDEX PK_테이블 ON 테이블 (ID) WITH PRIMARY
+ *
+ *     5. CREATE INDEX 문을 이용하여 테이블의 NULL 허용을 설정
+ *
+ *         CREATE INDEX IDX_테이블 ON 테이블 (이름) WITH DISALLOW NULL
+ *
+ *     6. 데이터 타입
+ *
+ *         문자열   : char, varchar, text     -> text
+ *         정수     : int, long               -> long
+ *         실수     : float, double           -> double
+ *         통화형   : currency
+ *         메모     : longtext
+ *         날짜     : datetime, timestamp     -> datetime
+ *         BLOB     : oleobject (RichEdit(RTF)는 이 형태를 써야함)
+ *         BOOLEAN  : YesNo
+ *
+ *     -------------------------------------------------------------------------------------------------------------------
+ *
+ *         CREATE TABLE table_name
+ *         (
+ *             attribute_1 data_type constraint,
+ *             attribute_2 data_type constraint,
+ *             ....
+ *         )
+ *
+ *
+ *         CREATE TABLE CUSTOMER
+ *         (
+ *             CustomerID  AUTOINCREMENT NOT NULL PRIMARY KEY,
+ *             LastName    varchar(25) NOT NULL,
+ *             FirstName   varchar(35) NOT NULL,
+ *             Address     varchar(35),
+ *             City        varchar(35),
+ *             State       varchar(2),
+ *             ZIP         varchar(10),
+ *             Phone       varchar(12) NOT NULL,
+ *             FAX         varchar(12),
+ *             Email       varchar(100)
+ *         )
+ *
+ *         CREATE TABLE CONTACT
+ *         (
+ *             ContactID   AUTOINCREMENT NOT NULL,
+ *             CustomerID  INT,
+ *             ContactDate DATETIME NOT NULL,
+ *             ContactType varchar(10)NOT NULL,
+ *             Remarks     LONGTEXT,
+ *
+ *             CONSTRAINT  CONTACT_PK  PRIMARY KEY(ContactID),
+ *             CONSTRAINT  CUS_CONT_FK FOREIGN KEY(CustomerID)
+ *                                      REFERENCES CUSTOMER(CustomerID)
+ *         )
+ *
+ *
+ *         ALTER TABLE table
+ *         {
+ *             ADD {COLUMN field type[(size)] [NOT NULL] [CONSTRAINT index] |
+ *             ALTER COLUMN field type[(size)] |
+ *             CONSTRAINT multifieldindex} |
+ *             DROP {COLUMN field I CONSTRAINT indexname}
+ *         }
+ *
+ *         ALTER TABLE table ADD COLUMN field type(size) (NOT NULL) (CONSTRAINT index)
  *
  */
 public class Example {
@@ -105,7 +211,15 @@ public class Example {
 	
 	private static Connection getUcanaccessConnection(String pathNewDB) throws SQLException, IOException {
 		
-		String url = UcanaccessDriver.URL_PREFIX + pathNewDB + ";newDatabaseVersion=V2003";
+		// 삭제
+		if (flag)
+			new File(pathNewDB).delete();
+		
+		/*
+		 * open access file or create access file
+		 */
+		String url = UcanaccessDriver.URL_PREFIX + pathNewDB + ";newDatabaseVersion=V2007";
+		log.debug(">>>>> url: " + url);
 		return DriverManager.getConnection(url, "sa", "");
 	}
 	
@@ -117,17 +231,70 @@ public class Example {
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	private void test() throws SQLException {
 	
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			stmt = this.ucaConn.createStatement();
+			stmt.execute("CREATE TABLE tab01 (seq LONG PRIMARY KEY, descr varchar(50)) ");
+			stmt.close();
+
+			this.ucaConn.setAutoCommit(false);
+			
+			stmt = this.ucaConn.createStatement();
+			
+			stmt.execute("INSERT INTO tab01 (seq, descr) VALUES ( 123, 'Bible' )");
+			stmt.execute("INSERT INTO tab01 (seq, descr) VALUES ( 456, 'Superman' )");
+
+			this.ucaConn.commit();
+			
+			rs = stmt.executeQuery("SELECT * from tab01");
+			dump(rs, "executeQuery");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null)
+				rs.close();
+			if (stmt != null)
+				stmt.close();
+		}
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	/*
+	 * Microsoft Access 프로그램에서 아래와 같이 작업 후에 _TESTDB01.accdb 새파일 작성한다.
+	 * 
+	 *     1. Access 옵션 > 기본 설정 > 새 데이터베이스 정렬 순서 : 일반 (General) 로 변경한다.
+	 *     2. 새파일 _TESTDB01.accdb 를 생성한다.
+	 *     
+	 */
 	private void createTablesExample() throws SQLException {
 		
 		Statement stmt;
 		
 		stmt = this.ucaConn.createStatement();
-		stmt.execute("CREATE TABLE example1 (id COUNTER PRIMARY KEY, descr text(400), number numeric(12,3), date0 datetime) ");
+		//stmt.execute("CREATE TABLE example1 (id COUNTER PRIMARY KEY, descr text(400), number numeric(12,3), date0 datetime) ");   // java OK!!, access No
+		//stmt.execute("CREATE TABLE example1 (id AUTOINCREMENT (1, 1) CONSTRAINT PK_EXAMPLE1 PRIMARY KEY, descr text(400), number numeric(12,3), date0 datetime) ");   // java error
+		//stmt.execute("CREATE TABLE example1 (id AUTOINCREMENT NOT NULL PRIMARY KEY, descr text(400), number numeric(12,3), date0 datetime) ");  // java OK!!! access No
+		//stmt.execute("CREATE TABLE example1 (id AUTOINCREMENT NOT NULL, descr text(400), number numeric(12,3), date0 datetime, CONSTRAINT PK_EXAMPLE1 PRIMARY KEY(id) ) ");  // java OK!!! access No
+		stmt.execute("CREATE TABLE example1 (id AUTOINCREMENT NOT NULL CONSTRAINT pk_example1 PRIMARY KEY, descr text(400), number numeric(12,3), date0 datetime ) ");  // java OK!!! access No
 		stmt.close();
 		
 		stmt = this.ucaConn.createStatement();
-		stmt.execute("CREATE TABLE example2 (id COUNTER PRIMARY KEY, descr text(400)) ");
+		//stmt.execute("CREATE TABLE example2 (id COUNTER PRIMARY KEY, descr text(400)) ");
+		stmt.execute("CREATE TABLE example2 (id AUTOINCREMENT NOT NULL PRIMARY KEY, descr text(400)) ");
 		stmt.close();
 		
 		stmt = this.ucaConn.createStatement();
@@ -197,7 +364,7 @@ public class Example {
 		try {
 			stmt = this.ucaConn.createStatement();
 			
-			ResultSet rs = stmt.executeQuery("SELECT llf(descr='Show must go off','tizio','caio&sempronio' & '&Macro Amadei' & ' ' & now() & RTRIM('  I''m proud of you   ')) from example1");
+			ResultSet rs = stmt.executeQuery("SELECT iif(descr='Show must go off','tizio','caio&sempronio' & '&Macro Amadei' & ' ' & now() & RTRIM('  I''m proud of you   ')) from example1");
 			dump(rs, "executeQueryWithFunctions");
 		} finally {
 			if (stmt != null)
@@ -347,6 +514,11 @@ public class Example {
 				
 				Example ex = new Example(args[0]);
 				
+				if (!flag) {
+					ex.test();
+					return;
+				}
+				
 				ex.createTablesExample();
 				ex.insertData();
 				ex.executeQuery();
@@ -357,7 +529,7 @@ public class Example {
 				ex.transaction();
 				
 			} catch (Exception e) {
-				// TODO: handle exception
+				e.printStackTrace();
 			}
 		}
 	}
@@ -371,7 +543,9 @@ public class Example {
 			log.debug(">>>>> " + new Object() {
 			}.getClass().getEnclosingClass().getName());
 		
-		if (flag)
-			test01(new String[] { "N:/" });
+		if (flag) {
+			//test01(new String[] { "G:/11. Matrix System ver1.0/_TESTDB01.accdb" });
+			test01(new String[] { "N:/_TESTDB01.accdb" });
+		}
 	}
 }
